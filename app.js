c59a0ec9718922ed8da46911bf36b8f83f703041
Arpan Thomas <66914302+Arpanthomas1121@users.noreply.github.com>
2023-03-04 02:29:31 +0530

    Update app.js
    Stupidity exists!

--- app.js
+++ app.js
@@ -6,6 +6,4 @@ const port = process.env.PORT || 3002;
 app.set('views', path.join(__dirname,"views"));
 app.set('view engine','ejs');
 app.use(indexRoutes)
-app.listen(3002, 'localhost', () => {
-  console.log('Backend server listening on port 3002');
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
